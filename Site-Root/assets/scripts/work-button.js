e5d72b8303314eab7ef19567323ab88e18ba354f
MaxMalts <max-kurt@bk.ru>
2020-08-16 14:59:22 +0300

    Created popup in work section

--- Site-Root/assets/scripts/work-button.js
+++ Site-Root/assets/scripts/work-button.js
@@ -1,109 +1,86 @@
-const transitionDuration = 800;  // in milliseconds
+var WorkButton_namespace = function () {
+	const transitionDuration = 800;  // in milliseconds
 
-function EaseInOutSine(x) {
-	return -(Math.cos(Math.PI * x) - 1) / 2;
-}
 
-function Animate(Timing, Draw, Duration, OnFinish = null) {
+	var ButtonStatus = {
+		loadMore: 0,
+		showLess: 1
+	};
 
-	let start = performance.now();
+	var button = document.getElementById("work__button");
+	var extraImages = document.getElementsByClassName("work__item_extra");
+	var buttonText = document.getElementById("button__text");
+	var workContent = document.getElementById("work__content");
 
-	requestAnimationFrame(function animate(time) {
-		// timeFraction изменяется от 0 до 1
-		let timeFraction = (time - start) / Duration;
-		if (timeFraction > 1) timeFraction = 1;
+	var startHeight, endHeight, curScrollY;
+	function SetHeight(progress) {
+		let curHeight = startHeight + progress * (endHeight - startHeight);
+		workContent.style.height = curHeight + "px";
+	
+		if (button.status == ButtonStatus.loadMore)
+			window.scrollTo(window.scrollX, curScrollY);
+	}
 
-		// вычисление текущего состояния анимации
-		let progress = Timing(timeFraction);
+	function UpdateScroll() {
+		curScrollY = window.scrollY;
+	}
 
-		Draw(progress); // отрисовать её
+	function AnimateWorkHeight(OnFinish = null) {
+		workContent.style.height = startHeight + "px";
 
-		if (timeFraction < 1)
-			requestAnimationFrame(animate);
-		else
+		window.addEventListener("scroll", UpdateScroll);
+
+		Animate(EaseInOutSine, SetHeight, transitionDuration, function () {
+			workContent.style.height = "auto";
 			if (OnFinish != null)
 				OnFinish();
-	});
-}
-
-
-var ButtonStatus = {
-	loadMore: 0,
-	showLess: 1
-};
-
-var button = document.getElementById("work__button");
-var extraImages = document.getElementsByClassName("work__item_extra");
-var buttonText = document.getElementById("button__text");
-var workContent = document.getElementById("work__content");
-
-var startHeight, endHeight, curScrollY;
-function SetHeight(progress) {	
-	let curHeight = startHeight + progress * (endHeight - startHeight);
-	workContent.style.height = curHeight + "px";
-	
-	if (button.status == ButtonStatus.loadMore)
-		window.scrollTo(window.scrollX, curScrollY);
-}
-
-function UpdateScroll() {
-	curScrollY = window.scrollY;
-}
-
-function AnimateWorkHeight(OnFinish = null) {
-	workContent.style.height = startHeight + "px";
-
-	window.addEventListener("scroll", UpdateScroll);
-
-	Animate(EaseInOutSine, SetHeight, transitionDuration, function () {
-		workContent.style.height = "auto";
-		if (OnFinish != null)
-			OnFinish();
-	});
-}
-
-function LoadMoreWork() {
-	for (let image of extraImages) {
-		image.style.display = "block";
-	}
-}
-
-function ShowLessWork() {
-	for (let image of extraImages) {
-		image.style.display = "none";
-	}
-}
-
-function OnButtonClick() {
-	if (button.status == ButtonStatus.loadMore) {
-		curScrollY = window.scrollY;
-
-		startHeight = workContent.scrollHeight;
-		LoadMoreWork();
-		endHeight = workContent.scrollHeight;
-
-		AnimateWorkHeight(function () {
-			buttonText.innerHTML = "show less";
-			button.status = ButtonStatus.showLess;
 		});
+	}
 
-	} else {
-		{
-			let t = startHeight;
-			startHeight = endHeight;
-			endHeight = t;
+	function LoadMoreWork() {
+		for (let image of extraImages) {
+			image.style.display = "block";
+		}
+	}
+
+	function ShowLessWork() {
+		for (let image of extraImages) {
+			image.style.display = "none";
+		}
+	}
+
+	function OnButtonClick() {
+		if (button.status == ButtonStatus.loadMore) {
+			curScrollY = window.scrollY;
+
+			startHeight = workContent.scrollHeight;
+			LoadMoreWork();
+			endHeight = workContent.scrollHeight;
+
+			AnimateWorkHeight(function () {
+				buttonText.innerHTML = "show less";
+				button.status = ButtonStatus.showLess;
+			});
+
+		} else {
+			{
+				let t = startHeight;
+				startHeight = endHeight;
+				endHeight = t;
+			}
+
+			AnimateWorkHeight(function () {
+				buttonText.innerHTML = "load more work";
+				button.status = ButtonStatus.loadMore;
+
+				ShowLessWork();
+			});
 		}
 
-		AnimateWorkHeight(function () {
-			buttonText.innerHTML = "load more work";
-			button.status = ButtonStatus.loadMore;
-
-			ShowLessWork();
-		});
 	}
 
+	button.status = ButtonStatus.loadMore;
+	button.addEventListener("click", OnButtonClick);
 }
 
-
-button.status = ButtonStatus.loadMore;
-button.addEventListener("click", OnButtonClick);
+WorkButton_namespace();
---
+++ Site-Root/assets/scripts/work-popup.js
@@ -0,0 +1,89 @@
+var WorkPopup_namespace = function () {
+
+	var popup = document.getElementById("work__popup");
+	var container = document.getElementById("work__popup__container");
+	var popupImage = document.getElementById("work__popup__image");
+	var content = document.getElementById("work__content");
+
+
+	function ThumbnailSrc(element) {
+		var error = new Error("element does not match a thumbnail template");
+		error.name = "NotThumbnailError";
+
+		var workItem = element.closest(".work__item");
+		if (!workItem)
+			throw error;
+		
+		var workImage = workItem.getElementsByClassName("work__image")[0];
+		if (!workImage)
+			throw error;
+		
+		return workImage.getAttribute("src");
+	}
+
+
+	function MatchedPopupSrc(thumbSrc) {
+		const postfix = "-large";  // added to end of file name
+
+		if (typeof thumbSrc != "string" || thumbSrc == "") {
+			throw new TypeError("bad thumbSrc argument");
+		}
+		
+		var dotInd = thumbSrc.indexOf('.');
+		if (dotInd == -1)
+			var res = thumbSrc + postfix;
+		else
+			var res = thumbSrc.slice(0, dotInd) + postfix + thumbSrc.slice(dotInd);
+
+		return res;
+	}
+
+
+	function ShowPopup(imgSrc) {
+		popupImage.setAttribute("src", imgSrc);
+
+		popup.style.display = "block";
+		CssAnimationIteration(popup);
+		CssAnimationIteration(container);
+
+		popupImage.addEventListener("load", function () {
+			FitImage(popupImage);
+		});
+	}
+
+
+	function HandleThumbClick(event) {
+		var clickedThumb = event.target;
+		var thumbSrc = ThumbnailSrc(clickedThumb);
+		if (!thumbSrc)
+			throw new Error("Bad src attribute on thumbnail");
+		
+		var popupSrc = MatchedPopupSrc(thumbSrc);
+
+		ShowPopup(popupSrc);
+	}
+
+
+
+	function HandlePopupClick() {
+		if (getComputedStyle(popup).animationPlayState == "paused") {
+			var listener = function () {
+				popup.style.display = "none";
+
+				popup.removeEventListener("animationiteration", listener);
+			}
+			popup.addEventListener("animationiteration", listener);
+
+			CssAnimationIteration(popup);
+			CssAnimationIteration(container);
+		}
+	}
+
+
+	content.addEventListener("click", HandleThumbClick);
+	popup.addEventListener("click", HandlePopupClick);
+
+}
+
+
+WorkPopup_namespace();
